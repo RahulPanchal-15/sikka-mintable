6370e4d0e4a7a2f1118bf7e8f53bc6d7218a85ae
RahulPanchal-15 <rahulkkm151@gmail.com>
2021-06-19 21:22:45 +0530

    frontend changes

---
+++ client/build/precache-manifest.5e43b64022271faf49c8650ef9aff7d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d545d88bad3d00492590463d072cd65a",
+    "url": "/sikka-mintable/index.html"
+  },
+  {
+    "revision": "a11fc4ace0933dc54046",
+    "url": "/sikka-mintable/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "63207149d043a51f21ed",
+    "url": "/sikka-mintable/static/js/2.a0ea077e.chunk.js"
+  },
+  {
+    "revision": "a11fc4ace0933dc54046",
+    "url": "/sikka-mintable/static/js/main.9536477a.chunk.js"
+  },
+  {
+    "revision": "d48b2335bb93899a1f1d",
+    "url": "/sikka-mintable/static/js/runtime-main.8dbe66ab.js"
+  }
+]);
---
+++ client/build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sikka-mintable/precache-manifest.5e43b64022271faf49c8650ef9aff7d8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sikka-mintable/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
