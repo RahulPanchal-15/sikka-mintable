93261cc58e20e7ea9ee9b945c45b8f9e68f592a1
RahulPanchal-15 <rahulkkm151@gmail.com>
2021-07-05 14:54:49 +0530

    Deployed

---
+++ client/build/precache-manifest.1382f1d14c4a39a12140ddc9ac28444b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67dd17b18acea890232627cb2398f302",
+    "url": "/sikka-mintable/index.html"
+  },
+  {
+    "revision": "6b5dfdcbd0da92d8b59c",
+    "url": "/sikka-mintable/static/css/main.3ffe561d.chunk.css"
+  },
+  {
+    "revision": "b01f807f17239bba6b92",
+    "url": "/sikka-mintable/static/js/2.9b217186.chunk.js"
+  },
+  {
+    "revision": "6b5dfdcbd0da92d8b59c",
+    "url": "/sikka-mintable/static/js/main.cb1e50f1.chunk.js"
+  },
+  {
+    "revision": "d48b2335bb93899a1f1d",
+    "url": "/sikka-mintable/static/js/runtime-main.8dbe66ab.js"
+  },
+  {
+    "revision": "700b900e047a6bcb97b87aa989568540",
+    "url": "/sikka-mintable/static/media/coin-mario.700b900e.gif"
+  }
+]);
